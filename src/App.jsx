b8ea9fa36fc58192a2c510f497b2b0c0f66a9750
sobhindra <sobhindra2005@gmail.com>
2026-09-03 20:25:58 +0545

    feat: implement routing with React Router and create Home, About, Contact, and NotFound pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { Main } from "./components/trello/mainTrello";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { NotFound } from "./pages/NotFound";
+
 
 function App() {
-  return <Main />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h1>hello everyone about</h1>} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
