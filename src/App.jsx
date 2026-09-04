4f21109101e90db78738dd5ff5433c49cfd55f04
sobhindra <sobhindra2005@gmail.com>
2026-09-04 18:16:39 +0545

    feat: reorganize page components into routing directory and update imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { About } from "./pages/About";
+import { Home } from "./pages/routing/Home";
+import { About } from "./pages/routing/About";
 import { NotFound } from "./pages/NotFound";
 
 
